2041c966624abe9dc780b6557ade1ea03a1bec5c
zhangjianfei <zhangjianfei@b.360.cn>
2017-12-01 16:23:37 +0800

    add fuck command

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,9 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('fuck', function () {
+    $ret = DB::table('weather')->get();
+    dump($ret);
+    echo "hello , fuck!";
+});
